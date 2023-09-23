04d9f6db81e059aa7ba592ec356b52e970b626bc
Shohei <shohei620528@gmail.com>
2023-09-23 14:02:29 +0900

    0923fix footer fix

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -5,48 +5,48 @@ import Link from 'next/link'
 export default function Footer(){
     return(
         <>
-            <div className="font-notosefif w-full text-center   text-sm  md:hidden bg-[url('/top/inner005.jpg')] bg-auto bg-center ">
-                <div className='backdrop-brightness-[0.2] py-[30px]'>
+            <div className="font-notosefif bg-[url('/top/inner005.jpg')] bg-auto bg-center ">
+                <div className='backdrop-brightness-[0.2] py-[30px]  w-full text-center   text-sm  md:hidden'>
 
                 {/* <img className='w-[200px] mx-auto' src='logo.png' /> */}
-                <Image  
+                    <Image  
                             src='/logo.png'
                             width={200} 
                             height={300} 
                             alt='Logo'
                             className='mx-auto'
                         />
-                <div className="mb-[30px]">
-                    <p>営業時間</p>
-                    <p>20:00～25:00</p>
+                    <div className="mb-[30px]">
+                        <p>営業時間</p>
+                        <p>20:00～25:00</p>
+                    </div>
+                    <div className="mb-[30px]">
+                        <p>定休日</p>
+                        <p>日曜日</p>
+                    </div>
+                    <div className="mb-[30px]">
+                        <p>店名</p>
+                        <p>ゴルフサロンうさぎ</p>
+                    </div>
+                    <div className="mb-[30px]">
+                        <p>所在地</p>
+                        <p>&#12306;810-0002</p>
+                        <p>福岡県福岡市中央区西中州1-14</p>
+                        <p>プロスペリタ西中洲7F</p>
+                    </div>
+                    <div className="mb-[30px]">
+                        <p>アクセス</p>
+                        <p>福岡市営下鉄天神南駅より徒歩４分</p>
+                    </div>
+                    <div className="mb-[30px]">
+                        <p>クレジット</p>
+                        <p>可（VISA,Master,JCB）</p>
+                    </div>
+                    <div className="mb-[40px]">
+                        <p>電子マネー</p>
+                        <p>可（PayPay）</p>
+                    </div>
                 </div>
-                <div className="mb-[30px]">
-                    <p>定休日</p>
-                    <p>日曜日</p>
-                </div>
-                <div className="mb-[30px]">
-                    <p>店名</p>
-                    <p>ゴルフサロンうさぎ</p>
-                </div>
-                <div className="mb-[30px]">
-                    <p>所在地</p>
-                    <p>&#12306;810-0002</p>
-                    <p>福岡県福岡市中央区西中州1-14</p>
-                    <p>プロスペリタ西中洲7F</p>
-                </div>
-                <div className="mb-[30px]">
-                    <p>アクセス</p>
-                    <p>福岡市営下鉄天神南駅より徒歩４分</p>
-                </div>
-                <div className="mb-[30px]">
-                    <p>クレジット</p>
-                    <p>可（VISA,Master,JCB）</p>
-                </div>
-                <div className="mb-[40px]">
-                    <p>電子マネー</p>
-                    <p>可（PayPay）</p>
-                </div>
-            </div>
 
             <div className="w-full hidden md:block bg-[#1b1715] font-notosefif ">
                 <div className='w-4/5 mx-auto flex justify-between py-[10px]'>
